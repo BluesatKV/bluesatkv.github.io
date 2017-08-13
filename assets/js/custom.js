836d26614e506094596645897096fe7ab2bf376f
BluesatKV <bluesatkv@gmail.com>
2017-08-13 10:09:31 +0200

    ...

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,24 +1,42 @@
-function loadContent(hash) {
-  if (hash === '') {
+(function($) {
+
+  /** @function loadContent
+   * @description Load content by href links
+   * @requires nothing
+   */
+
+  function loadContent(hash) {
+    if (hash === '') {
+      hash = 'home';
+    }
+
+    $('html, body').animate({scrollTop: 0}, '600', 'swing');
+    $('section').load('includes/' + hash + '.html');
+  }
+
+  $(window).on('hashchange', function () {
+    loadContent(location.hash.slice(1));
+  });
+
+  var url = window.location.href;
+  var hash = url.substring(url.indexOf("#") + 1);
+
+  if (hash === url) {
     hash = 'home';
   }
 
-  $('html, body').animate({scrollTop: 0}, '600', 'swing');
   $('section').load('includes/' + hash + '.html');
-}
+  $('header').load('includes/header.html');
+  $('footer').load('includes/footer.html');
 
-$(window).on('hashchange', function () {
-  loadContent(location.hash.slice(1));
-});
+  /** @function Change theme
+   * @description Change theme on click by button
+   * @requires nothing
+   */
 
-var url = window.location.href;
-var hash = url.substring(url.indexOf("#") + 1);
+  $('#theme-select a').on('click', function(event) {
+    event.preventDefault();
+    $('#theme-file').attr('href', $(this).attr('href'));
+  });
 
-if (hash === url) {
-  hash = 'home';
-}
-
-$('section').load('includes/' + hash + '.html');
-
-$('header').load('includes/header.html');
-$('footer').load('includes/footer.html');
+})(window.jQuery);
